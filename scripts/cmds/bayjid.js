103dbab4194eb2e91c6ef684cb41f895c9c9c5d5
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-02-01 21:27:30 +0600

    Update bayjid.js

--- scripts/cmds/bayjid.js
+++ scripts/cmds/bayjid.js
@@ -1,22 +1,22 @@
 module.exports = {
- config: {
-	 name: "bayjid",
-	 version: "1.0",
-	 author: "AceGun",
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+config: {
+name: "bayjid",
+version: "1.0",
+author: "AceGun",
+countDown: 5,
+role: 0,
+shortDescription: "no prefix",
+longDescription: "no prefix",
+category: "no prefix",
+},
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "ayan") {
- return message.reply({
- body: " 「❥︎----ღ᭄_ᴬˢˢᴬᴸᴬᴹᴼᴸᴬᴵᴷᵁᴹ ..\n❥︎----ღ᭄_  .🌴.\n❥ \n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 Ă̈Y̆̈Ă̈N̆̈」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/IN0CM0T.mp4")
- });
- }
- }
+onStart: async function(){}, 
+onChat: async function({ event, message, getLang }) {
+if (event.body && event.body.toLowerCase() === "ayan") {
+return message.reply({
+body: " 「├───【𝙷𝙴𝙻𝙻𝙾 𝙴𝚅𝙴𝚁𝚈𝙾𝙽𝙴】─⦿\n❥ \n\n├─BoT Owner:【Ă̈Y̆̈Ă̈N̆̈】\n├─MIKEY bbz:【Ă̈Y̆̈Ă̈N̆̈】」",
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/IN0CM0T.mp4")
+});
+}
+}
 }
