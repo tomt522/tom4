1900504eb54cbb0e1fc1557a43f6ad13a6e4db96
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-02-01 21:26:39 +0600

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,93 +1,57 @@
 const axios = require('axios');
-const tracker = {};
+const UPoLPrefix = [
+  '-ai',
+  'ai',
+  '/ai',
+  '',
+  'ask'
+]; 
 
-/*
-A Turtle APIs Production created by Turtle Rehat.
-Do not alter the credited information any attempt to do so may result in a permanent ban from Project86 APIs and Turtle APIs.
-*/
+  module.exports = {
+  config: {
+    name: 'ai',
+    version: '1.2.1',
+    role: 0,
+    category: 'AI',
+    author: 'ncs pro',
+    shortDescription: '',
+    longDescription: '',
+  },
+  
+  onStart: async function () {},
+  onChat: async function ({ message, event, args, api, threadID, messageID }) {
+      
+      const ahprefix = UPoLPrefix.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!ahprefix) {
+        return; 
+      } 
+      
+     const upol = event.body.substring(ahprefix.length).trim();
+   if (!upol) {
+        await message.reply('Enter a question.? 🥹');
+        return;
+      }
+      
+      const apply = ['Awww🥹, maybe you need my help', 'How can i help you?', 'How can i assist you today?', 'How can i help you?🙂'];
+      
+     const randomapply = apply[Math.floor(Math.random() * apply.length)];
 
-module.exports = {
-	config: {
-		name: "bayot",
-		version: "1.0",
-		author: "rehat--",
-		countDown: 5,
-		role: 0,
-		longDescription: "Chat GPT 4 Most Advance LLM",
-		category: "ai",
-		guide: { en: "{pn} <query>" },
-	},
-	clearHistory: function () {
-		global.GoatBot.onReply.clear();
-	},
-	onStart: async function ({ message, event, args, usersData, api, commandName }) {
-		const prompt = args.join(' ');
-		const userID = event.senderID;
-		const mid = event.messageID;
+     
+      if (args[0] === 'hi') {
+          message.reply(`${randomapply}`);
+          return;
+      }
+      
+    const encodedPrompt = encodeURIComponent(args.join(" "));
 
-		if (!args[0]) return message.reply('Please enter a query.');
+   await message.reply('princes  thinking..');
+  
+    const response = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodedPrompt}`);
+ 
+     const UPoL = response.data.answer; 
 
-		if (args[0] == 'clear') {
-			this.clearHistory();
-			const c = await clean(userID);
-			if (c) return message.reply('Conversation history cleared.');
-		}
-
-		api.setMessageReaction('⏳', mid, () => {}, true);
-		gpt(prompt, userID, message, mid, api);
-	},
-
-	onReply: async function ({ Reply, message, event, args, api, usersData }) {
-		const { author } = Reply;
-		if (author !== event.senderID) return;
-
-		const mid = event.messageID;
-		const prompt = args.join(' ');
-		const userID = event.senderID;
-
-		if (args[0] == 'clear') {
-			this.clearHistory();
-			const c = await clean(userID);
-			if (c) return message.reply('Conversation history cleared.');
-		}
-
-		api.setMessageReaction('⏳', mid, () => {}, true);
-		gpt(prompt, userID, message, mid, api);
-	}
+      const upolres = `${UPoL}`;
+      
+        message.reply(upolres);
+  }
 };
-
-async function clean(userID) {
-	if (!tracker[userID]) return true;
-	if (tracker[userID]) {
-		delete tracker[userID];
-		return true;
-	}
-}
-
-async function gpt(text, userID, message, mid, api) {
-	tracker[userID] = tracker[userID] || '';
-	tracker[userID] += `${text}.\n`;
-
-	try {
-		const url = 'https://project86.cyclic.app/api/chat';
-
-		const conversationHistory = encodeURIComponent(tracker[userID]);
-		const getUrl = `${url}?query=${conversationHistory}`;
-
-		const response = await axios.post(getUrl);
-
-		const resultText = response.data.answer;
-		tracker[userID] = `${tracker[userID]}${text}.\n${resultText}`;
-
-		api.setMessageReaction('✅', mid, () => {}, true);
-		message.reply(`${resultText}\n\n𝙔𝙤𝙪 𝙘𝙖𝙣 𝙧𝙚𝙥𝙡𝙮 𝙩𝙤 𝙘𝙤𝙣𝙩𝙞𝙣𝙪𝙚 𝙘𝙝𝙖𝙩𝙩𝙞𝙣𝙜.`, (error, info) => {
-			global.GoatBot.onReply.set(info.messageID, {
-				commandName: 'ai',
-				author: userID,
-			});
-		});
-	} catch (error) {
-		api.setMessageReaction('❌', mid, () => {}, true);
-		message.reply('An error occurred.');
-	}
-}
