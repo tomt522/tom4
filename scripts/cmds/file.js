49f27a455b9e785607c422153ffca4002a3fce37
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-02-07 00:58:38 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100091369382921",];
+		const permission = ["61572491867501",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
