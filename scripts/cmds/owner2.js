847b525d82de2c6088cc3898dfaa4b46aba893d0
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-02-10 19:55:12 +0600

    Update owner2.js

--- scripts/cmds/owner2.js
+++ scripts/cmds/owner2.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
 config: {
-  name: "owner",
+  name: "owner2",
   aurthor:"Tokodori",// Convert By Goatbot Tokodori 
    role: 0,
   shortDescription: " ",
