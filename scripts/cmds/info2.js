0f5cdc4c2b5298afa795116075ab79654a27457f
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-02-10 06:09:36 +0600

    Create info2.js

---
+++ scripts/cmds/info2.js
@@ -0,0 +1,72 @@
+const fs = require('fs');const moment = require('moment-timezone');
+module.exports = {
+  config: {
+    name: "info",
+    aliases: ["inf", "in4"],
+    version: "2.0",
+    author: "VEX_ADNAN",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin along with an image."
+    },
+    longDescription: {
+      vi: "",
+      en: "Sends information about the bot and admin along with an image."
+    },
+    category: "Information",
+    guide: {
+      en: "{pn}"
+    },
+    envConfig: {}
+  },
+
+  onStart: async function ({ message }) {
+    this.sendInfo(message);
+  },
+
+  onChat: async function ({ event, message }) {
+    if (event.body && event.body.toLowerCase() === "info") {
+      this.sendInfo(message);
+    }
+  },
+
+  sendInfo: async function (message) {
+    const botName = " 🕸️ SpideY 🕷️ ";
+    const botPrefix = ".";
+    const authorName = "Mahid";
+    const authorFB = "LaFie Nadim";
+    const authorInsta = "Shor Mgii 😒";
+    const status = "Loading......";
+
+    const urls = JSON.parse(fs.readFileSync('scripts/cmds/assets/Ayan.json'));
+    const link = urls[Math.floor(Math.random() * urls.length)];
+
+    const now = moment().tz('Asia/Dhaka');
+    const date = now.format('MMMM Do YYYY');
+    const time = now.format('h:mm:ss A');
+
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${hours}h ${minutes}m ${seconds}sec`;
+
+    message.reply({
+      body: `╭────────────◊
+├‣ 𝐁𝐨𝐭 & 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 
+├‣ 𝐍𝐚𝐦𝐞: ${authorName}
+├‣ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞:  ${botName}
+├‣ 𝐏𝐫𝐞𝐟𝐢𝐱:  ${botPrefix}
+├‣ 𝐅𝐛: ${authorFB}
+├‣ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦:  ${authorInsta}
+├‣ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩: ${status}   
+├‣ 𝐓𝐢𝐦𝐞:  ${time}
+├‣ 𝐔𝐩𝐭𝐢𝐦𝐞: ${uptimeString}
+╰────────────◊`,
+      attachment: await global.utils.getStreamFromURL(link)
+    });
+  }
+};
