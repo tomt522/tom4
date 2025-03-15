a30c2adeb644db6986df69766f5b260f50b3785f
tomt522 <rifatjamil613@gmail.com>
2025-03-15 16:43:49 +0600

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61572491867501"];
+    const permission = ["61559427406776"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
     return;
