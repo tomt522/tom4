663e1272950f404de73f1e7777efeaf90f972406
tomt522 <rifatjamil613@gmail.com>
2025-03-16 13:30:37 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -58,7 +58,7 @@ module.exports = {
 
       const totalCommands = commands.size;
       msg += \n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n;
-      msg += `\n𝗧𝘆𝗽𝗲 ${prefix}𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 \n🫧𝘽𝙊𝙏 𝙉𝘼𝙈𝙀🫧:𝐓𝐎𝐌🎀+= `\n🫧𝘽𝙊𝙏 𝙉𝘼𝙈𝙀🫧:\n🔹 𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍 🔹    msg += `\n🔹 𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍\n 	 					sg += `\n 	 			\n~𝙉𝘼𝙈𝙀:✰𝙍𝙞𝙛𝙖𝙩 𝙅𝙖𝙢𝙞𝙡 ✰:✰𝙍𝙞𝙛𝙖𝙩 𝙅𝙖𝙢𝙞𝙡 ✰`;
+      msg += `\n𝗧𝘆𝗽𝗲 ${prefix}𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 \n🫧𝘽𝙊𝙏 𝙉𝘼𝙈𝙀🫧:𝐓𝐎𝐌🎀+= `\n🫧𝘽𝙊𝙏 𝙉𝘼𝙈𝙀🫧:\n🔹 𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍 🔹 RIFAT   msg += `\n🔹 𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍\n 	 					sg += `\n 	 			\n~𝙉𝘼𝙈𝙀:✰𝙍𝙞𝙛𝙖𝙩 𝙅𝙖𝙢𝙞𝙡 ✰:✰𝙍𝙞𝙛𝙖𝙩 𝙅𝙖𝙢𝙞𝙡 ✰`;
       msg +\n~𝙁𝘽:https://www.facebook.com/share/166VHaYd36//`;
 
       await message.reply({
